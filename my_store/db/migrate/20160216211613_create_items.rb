0f538d19a4096522bc09759950b3b65988fd7891
depytat <ziabko4488@gmail.com>
2016-02-18 10:18:53 +0300

    second

--- my_store/db/migrate/20160216211613_create_items.rb
+++ my_store/db/migrate/20160216211613_create_items.rb
@@ -1,7 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-
+      t.float   :price
+      t.string  :name
+      t.boolean :real
+      t.float   :weight
       t.timestamps null: false
     end
   end
