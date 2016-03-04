e880c85ae4f1eb90cdc0ebff27c4bcdd810ddf54
d.zyabko <d.zyabko@itransition.com>
2016-03-04 12:45:28 +0300

    106 страница на cart items size

--- my_store/app/models/position.rb
+++ my_store/app/models/position.rb
@@ -1,5 +1,5 @@
 class Position < ActiveRecord::Base
-	attr_accessible :cart, :cart_id, :item, :item_id, :quantity
+	# attr_accessible :cart, :cart_id, :item, :item_id, :quantity
 	belongs_to :item
 	belongs_to :cart
 end
